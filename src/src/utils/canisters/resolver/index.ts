0b98a90f0d6253a5f00ccdc76c5d0c454f5b861d
Roy <royandroll@outlook.com>
2022-02-25 17:09:23 +0800

    1.0 support icp production and ticp test net

--- src/src/utils/canisters/resolver/index.ts
+++ src/src/utils/canisters/resolver/index.ts
@@ -1,25 +1,12 @@
 import { actorFactory } from "../actorFactory";
 import { _SERVICE } from "./interface";
 import { idlFactory } from "./did";
-import { getResolverId } from "./canisterId";
 import { Principal } from "@dfinity/principal";
 
 export type ResolverActor = _SERVICE;
-
-export const createResolverQueryActor = () =>
-  actorFactory.createActorWithAnonymousIdentity<ResolverActor>(
-    idlFactory,
-    getResolverId()
-  );
 
 export const createResolverQueryActorById = (resolver: Principal) =>
   actorFactory.createActorWithAnonymousIdentity<ResolverActor>(
     idlFactory,
     resolver
-  );
-
-export const createResolverUpdateActor = () =>
-  actorFactory.createActor<ResolverActor>(
-    idlFactory,
-    getResolverId()
   );
--- src/src/utils/config.ts
+++ src/src/utils/config.ts
@@ -1,64 +1,17 @@
 const IC_MAINNET_HOST = "https://ic0.app";
 const IC_LOCAL_HOST = "http://127.0.0.1:8000";
-let env = "MainNet";
 const isLocalEnv = (): boolean => {
   return process.env.EXTENSION_ENV === "Local";
 };
 const isTestNetEnv = (): boolean => {
-  return env == "TestNet";
+  return process.env.EXTENSION_ENV === "TestNet";
+
 };
 const isMainNetEnv = (): boolean => {
-  return env == "MainNet";
+  return process.env.EXTENSION_ENV === "MainNet";
 };
 
-const set_env = (env: string) => {
-  chrome.storage.local.set({ extension_env: env }, () => {
-    console.log("set env to " + env);
-  });
-};
-
-const load_env_with_call = (callback: (env: string) => void) => {
-  chrome.storage.local.get("extension_env", (result) => {
-    if (result.extension_env) {
-      env = result.extension_env;
-    }
-    callback(env);
-  });
-};
-
-const load_env = () => {
-  chrome.storage.local.get("extension_env", (result) => {
-    if (result.extension_env) {
-      console.log("env is " + result.extension_env);
-      env = result.extension_env;
-    } else {
-      env = "MainNet";
-    }
-  });
-
-  chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (namespace == "local" && changes.extension_env) {
-      env = changes.extension_env.newValue;
-      console.log("env changed to " + env);
-    }
-  });
-};
-
-load_env();
-
-const get_env = (callback: (env: string) => void): void => {
-  chrome.storage.local.get("extension_env"
-    , (result) => {
-      if (result.extension_env) {
-        console.log("env is " + result.extension_env);
-        env = result.extension_env;
-      } else {
-        env = "MainNet";
-      }
-      callback(env);
-    });
-};
 
 const IC_HOST = isLocalEnv() ? IC_LOCAL_HOST : IC_MAINNET_HOST;
-export { IC_HOST, isLocalEnv, isTestNetEnv, isMainNetEnv, set_env, get_env, load_env_with_call };
+export { IC_HOST, isLocalEnv, isTestNetEnv, isMainNetEnv, };
 
