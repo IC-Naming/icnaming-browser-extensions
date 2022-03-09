2003fe3332af2f2518e691e61e6a294fc33205a7
Roy <royandroll@outlook.com>
2022-03-09 23:00:34 +0800

    support yo redirect to url when there is no canister

--- src/src/background.ts
+++ src/src/background.ts
@@ -1,4 +1,4 @@
-import { get_redirect_host, NameEnv } from "./utils/ic_naming";
+import {get_redirect_host, NameEnv, get_redirect_to, RedirectInfo} from "./utils/ic_naming";
 
 const rule_id_start = 1000;
 let current_rule_id = rule_id_start;
@@ -11,15 +11,15 @@ let get_env_rules = (name_env: NameEnv) => {
 };
 
 
-let save_rule = async (name: string, redirect_host: string, name_env: NameEnv): Promise<string> => {
+let save_rule = async (name: string, redirect_host: RedirectInfo, name_env: NameEnv): Promise<RedirectInfo> => {
   let result = get_env_rules(name_env);
   if (result[name]) {
     let cache = result[name];
-    cache["redirect_host"] = redirect_host;
+    cache["redirect_info"] = redirect_host;
   } else {
     let id = current_rule_id++;
     result[name] = {
-      "redirect_host": redirect_host,
+      "redirect_info": redirect_host,
       "rule_id": id
     };
 
@@ -28,17 +28,16 @@ let save_rule = async (name: string, redirect_host: string, name_env: NameEnv): 
   return redirect_host;
 };
 
-let get_rule_host = (name: string, name_env: NameEnv): string => {
+let get_redirect_info = (name: string, name_env: NameEnv): RedirectInfo | null => {
   let result = get_env_rules(name_env);
   if (result[name]) {
-    let cache = result[name];
-    return cache["redirect_host"];
+    return result[name]["redirect_info"] ;
   }
-  return "";
+  return null;
 };
 
-let upsert_redirect_url = async (name: string, name_env: NameEnv): Promise<string> => {
-  let redirect_host = get_rule_host(name, name_env);
+let upsert_redirect_info = async (name: string, name_env: NameEnv): Promise<RedirectInfo> => {
+  let redirect_host = get_redirect_info(name, name_env);
   if (redirect_host) {
     console.log(`${name} already has a redirect host: ${redirect_host}`);
     return redirect_host;
@@ -59,21 +58,17 @@ let urlHandler = (name_env: NameEnv, details: any) => {
   let hostname = new URL(details.url).hostname;
   console.log("onBeforeRequest", hostname);
   if (hostname.endsWith(".icp") || hostname.endsWith(".ticp")) {
-    let redirect_host = get_rule_host(hostname, name_env);
+    let redirect_host = get_redirect_info(hostname, name_env);
     if (redirect_host) {
-      // replace the hostname with the redirect host
-      // and update schema to https
-      let redirect_url = new URL(details.url);
-      redirect_url.hostname = redirect_host;
-      redirect_url.protocol = "https:";
-      return { redirectUrl: redirect_url.toString() };
+      let redirectTo = get_redirect_to(details.url, redirect_host);
+      return {redirectUrl: redirectTo};
     } else {
-      let result = upsert_redirect_url(hostname, name_env);
+      let _ = upsert_redirect_info(hostname, name_env);
       // redirect to redirect.html with query string
       // get redirect.html url from extensions
       let redirect_url_base = chrome.runtime.getURL("redirect.html");
       let url = `${redirect_url_base}?source=${encodeURIComponent(details.url)}&env=${name_env}`;
-      return { redirectUrl: url };
+      return {redirectUrl: url};
     }
   }
 };
--- src/src/utils/ic_naming.ts
+++ src/src/utils/ic_naming.ts
@@ -1,7 +1,8 @@
 import ServiceApi from "./ServiceApi";
-import { Principal } from "@dfinity/principal";
+import {Principal} from "@dfinity/principal";
 
 const canister_id_key = "canister.icp";
+const url_key = "url";
 
 enum NameEnv {
   MainNet = "MainNet",
@@ -19,20 +20,48 @@ let get_registrar = (name_env: NameEnv): Principal => {
   }
 };
 
-let get_redirect_host = async (name: string, name_env: NameEnv): Promise<string> => {
+interface RedirectInfo {
+  name: string;
+  url: string;
+  redirect_host: string;
+}
+
+let get_redirect_host = async (name: string, name_env: NameEnv): Promise<RedirectInfo> => {
   let serviceApi = new ServiceApi();
   let resolver = await serviceApi.getResolverOfName(name, get_registrar(name_env));
   if (resolver) {
     let values = await serviceApi.getRecordsOfName(name, resolver);
     if (values) {
       console.log(values);
-      let redirect_url = values.find((value) => value[0] === canister_id_key);
-      if (redirect_url) {
-        return `${redirect_url[1]}.raw.ic0.app`;
-      }
+      let canister_id = values.find((value) => value[0] === canister_id_key);
+      let url = values.find((value) => value[0] === url_key);
+      return {
+        name: name,
+        url: url ? url[1] : "",
+        redirect_host: canister_id ? `${canister_id[1]}.raw.ic0.app` : "",
+      };
     }
   }
-  return "";
+  return {
+    name: name,
+    url: "",
+    redirect_host: "",
+  };
 };
 
-export { canister_id_key, get_redirect_host, NameEnv };
+const get_redirect_to = (url: string, redirect_info: RedirectInfo): string => {
+  let redirect_url = new URL(url);
+  if (redirect_info.redirect_host) {
+    // replace the hostname with the redirect host
+    // and update schema to https
+    redirect_url.hostname = redirect_info.redirect_host;
+    redirect_url.protocol = "https:";
+    return redirect_url.toString();
+  }
+  if (redirect_info.url) {
+    return redirect_info.url;
+  }
+  return `https://app.icnaming.com/search/${redirect_url.hostname}`;
+}
+
+export {canister_id_key, get_redirect_host, RedirectInfo, get_redirect_to, NameEnv};
