cbee23c0592cd1f599822987a55748cf3e619fe0
Kevin-icnaming <95837832+Kevin-icnaming@users.noreply.github.com>
2022-07-27 18:03:35 +0800

    [update] options UI (#71)
    * [update] options UI
    
    * add options_page to mainifest.json
    
    * version 2.0.3
    
    Co-authored-by: vanilla <104433129+extabr@users.noreply.github.com>

--- src/src/options.tsx
+++ src/src/options.tsx
@@ -1,70 +1,35 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 const Options = () => {
-    const [color, setColor] = useState<string>("");
-    const [status, setStatus] = useState<string>("");
-    const [like, setLike] = useState<boolean>(false);
-
-    useEffect(() => {
-        // Restores select box and checkbox state using the preferences
-        // stored in chrome.storage.
-        chrome.storage.sync.get(
-            {
-                favoriteColor: "red",
-                likesColor: true,
-            },
-            (items) => {
-                setColor(items.favoriteColor);
-                setLike(items.likesColor);
-            }
-        );
-    }, []);
-
-    const saveOptions = () => {
-        // Saves options to chrome.storage.sync.
-        chrome.storage.sync.set(
-            {
-                favoriteColor: color,
-                likesColor: like,
-            },
-            () => {
-                // Update status to let user know options were saved.
-                setStatus("Options saved.");
-                const id = setTimeout(() => {
-                    setStatus("");
-                }, 1000);
-                return () => clearTimeout(id);
-            }
-        );
-    };
-
+    const wrapStyle = {
+        width:"425px",
+        BoxSizing: "border-box", 
+        margin:"0 auto",
+        padding: "1rem"
+    }
+    const logoStyle = {
+        width:"2rem",
+        height: "2rem",
+        margin:"5px 10px 0 0"
+    }
+    const fontStyle = {
+        color: "rgba(88,98,122,0.5)",
+        fontSize: "0.8rem",
+    }
     return (
-        <>
-            <div>
-                Favorite color: <select
-                value={color}
-                onChange={(event) => setColor(event.target.value)}
-            >
-                <option value="red">red</option>
-                <option value="green">green</option>
-                <option value="blue">blue</option>
-                <option value="yellow">yellow</option>
-            </select>
-            </div>
-            <div>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={like}
-                        onChange={(event) => setLike(event.target.checked)}
-                    />
-                    I like colors.
-                </label>
-            </div>
-            <div>{status}</div>
-            <button onClick={saveOptions}>Save</button>
-        </>
+        <div style={wrapStyle}>
+		<div style={{display: "flex"}}>
+			<img style={logoStyle} src="https://cdn.jsdelivr.net/gh/IC-Naming/icnaming-dapp@main/public/favicon.svg" alt="logo" />
+	        <div>
+	        	<strong>The gate of decentralized web</strong>
+	        	<div style={fontStyle}>This extension opens websites on the blockchain</div>
+	        	<div style={{textAlign: "right",marginTop: "1rem"}}>
+		            <a style={fontStyle} href="https://app.icnaming.com/">Register a decentralized name of your own</a>
+		        </div>
+	        </div>
+		</div>
+    </div>
     );
 };
 
